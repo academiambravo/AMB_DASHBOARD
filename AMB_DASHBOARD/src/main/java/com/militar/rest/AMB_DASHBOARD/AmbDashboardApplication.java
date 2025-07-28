347b32a4579694d0fca9ab8a2c1cddeebad0ad6e
carlitosromvn <carlosromanabad@gmail.com>
2025-07-28 09:23:10 +0200

    add springboot project

---
+++ AMB_DASHBOARD/src/main/java/com/militar/rest/AMB_DASHBOARD/AmbDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.militar.rest.AMB_DASHBOARD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbDashboardApplication.class, args);
+	}
+
+}
---
+++ AMB_DASHBOARD/src/test/java/com/militar/rest/AMB_DASHBOARD/AmbDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.militar.rest.AMB_DASHBOARD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
